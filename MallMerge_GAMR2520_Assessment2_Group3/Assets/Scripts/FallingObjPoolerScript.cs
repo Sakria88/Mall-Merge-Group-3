df55b99d1a2ba276143057602425e26e286733ff
Iker Cubillo <i.cubillo@opendeusto.es>
2026-01-19 03:24:59 +0000

    changes

--- MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/FallingObjPoolerScript.cs
+++ MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/FallingObjPoolerScript.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformPoolerScript : MonoBehaviour
+public class FallingObjPoolerScript : MonoBehaviour
 {
-    public GameObject[] platformPrefabs; // Array of platform prefabs to pool
+    public GameObject[] objectPrefabs; // Array of object prefabs to pool
     public int poolSize; // Number of platforms to pool
     public int startPrefabs;
     public Dictionary<GameObject, bool> pool = new Dictionary<GameObject, bool>(); // Pool dictionary
@@ -17,11 +17,11 @@ public class PlatformPoolerScript : MonoBehaviour
         
         for (int i = 0; i < poolSize; i++)
         {
-            obj = Instantiate(platformPrefabs[Random.Range(0, platformPrefabs.Length)],
+            obj = Instantiate(objectPrefabs[Random.Range(0, objectPrefabs.Length)],
                               tempPosition.position,
                               Quaternion.identity); // Instantiate at temp position
 
-            pool.Add(obj, true); // true indicates the platform is available
+            pool.Add(obj, true); // true indicates the object is available
         }
 
         for (int i = 0; i < startPrefabs; i++)
@@ -34,13 +34,13 @@ public class PlatformPoolerScript : MonoBehaviour
     {
         foreach (KeyValuePair<GameObject, bool> obj in pool)
         {
-            if (obj.Value) // If the platform is available
+            if (obj.Value) // If the object is available
             {
                 pool[obj.Key] = false; // Mark as in use
                 obj.Key.transform.position = spawnPosition.position; // Move to spawn position
                 NextSpawnPositionCal(obj.Key); // Calculate next spawn position
-                StartCoroutine(ReturnPrefab(obj.Key)); // Start coroutine to return platform to pool
-                break; // Exit after spawning one platform
+                StartCoroutine(ReturnPrefab(obj.Key)); // Start coroutine to return object to pool
+                break; // Exit after spawning one object
             }
         }
     }
@@ -49,23 +49,23 @@ public class PlatformPoolerScript : MonoBehaviour
     {
         spawnPosition.position = new Vector2(Random.Range(-2.8f, 2.8f),
                                              selPrefab.transform.position.y +
-                                             Random.Range(2.1f, 2.75f)); // Update spawn position for next platform
+                                             Random.Range(2.1f, 2.75f)); // Update spawn position for next object
     }
 
     IEnumerator ReturnPrefab(GameObject selPrefab)
     {
         while (!selPrefab.GetComponent<Renderer>().isVisible)
         {
-            yield return new WaitForEndOfFrame(); // Wait until the platform is visible
+            yield return new WaitForEndOfFrame(); // Wait until the object is visible
         }
         while (selPrefab.GetComponent<Renderer>().isVisible)
         {
-            yield return new WaitForEndOfFrame(); // Wait until the platform is no longer visible
+            yield return new WaitForEndOfFrame(); // Wait until the object is no longer visible
         }
 
         selPrefab.transform.position = tempPosition.position; // Move back to temp position
         pool[selPrefab] = true; // Mark as available
 
-        SpawnPrefab(); // Spawn a new platform
+        SpawnPrefab(); // Spawn a new object
     }
 }
