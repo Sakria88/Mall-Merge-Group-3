d675ad06006f1656c892754d460a24893524f003
Sakria88 <sakriaobido@gmail.com>
2026-01-25 16:24:10 +0000

    fixing play area

--- MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/MenuBridge.cs
+++ MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/MenuBridge.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuBridge : MonoBehaviour
 {
-    // A helper to find the active MenuManager instance
-    private MenuManager GetManager()
-    {
-        return FindObjectOfType<MenuManager>();
-    }
+    // Helpers to find the active scripts in the current scene
+    private MenuManager GetManager() => FindObjectOfType<MenuManager>();
+    private PlayAreaNavigation GetNav() => FindObjectOfType<PlayAreaNavigation>();
 
+    // --- FOR THE MAIN MENU SCENE ---
+    // Use these for buttons physically located in the "MallMergeMenus" scene
     public void OpenPlay() => GetManager().OnPlayButtonClicked();
     public void OpenSettings() => GetManager().OnSettingsButtonClicked();
     public void OpenShop() => GetManager().OnShopButtonClicked();
     public void OpenHelp() => GetManager().OnHelpButtonClicked();
     public void BackToMain() => GetManager().BackToMainMenu();
+
+    // --- FOR THE GAMEPLAY SCENE (MainGamePlayArea) ---
+    // Use these for buttons physically located in the "MainGamePlayArea" scene
+    // These use your PlayAreaNavigation logic to switch scenes and set the target panel
+    public void GameToMenuSettings() => GetNav().GoToMenuSettings();
+    public void GameToMenuShop() => GetNav().GoToMenuShop();
+    public void GameToMenuHelp() => GetNav().GoToMenuHelp();
+    public void GameToMainMenu() => GetNav().GoToMainMenu();
 }
