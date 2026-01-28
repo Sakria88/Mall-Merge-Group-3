1e39f6b1e0bd359a4016647409be9a1e5b22bc27
Sakria88 <sakriaobido@gmail.com>
2026-01-28 06:59:04 +0000

    tried adding animator failed

--- MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/GameManagerScript.cs
+++ MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/GameManagerScript.cs
@@ -87,9 +87,11 @@ public class GameManagerScript : MonoBehaviour
         //Debug.Log("Cost: " + starCost);
 
         if (!SpendStars(starCost))
+        {
+            Debug.Log("NOT ENOUGH STARS");
             return false;
-        Debug.Log("NOT ENOUGHS");
-
+        }
+        
         AddEnergy(gainEnergy);
         Debug.Log("Purchase Success");
         return true;
--- MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/MiniGame/BombObjectScript.cs
+++ MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/MiniGame/BombObjectScript.cs
@@ -1,14 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BombObjectScript : FallingObjectScript
 {
-    public int energyValue = -5;
+    private int energyValue = -5;
+    private GameObject explosionPrefab;   
+    private Vector3 explosionOffset = Vector3.zero;
+
+    private bool hasExploded = false;
 
     protected override void OnCollected()
     {
-        Debug.Log("Boooooom!" + " Value: " + energyValue);
+        if (hasExploded) return;
+        hasExploded = true;
+
+        Debug.Log("Boooooom! Value: " + energyValue);
+
+    
+        if (explosionPrefab != null)
+        {
+            Instantiate(explosionPrefab, transform.position + explosionOffset, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("BombObjectScript: explosionPrefab is NOT assigned on " + gameObject.name);
+        }
+
+        // Return bomb to pool / existing collected logic
         base.OnCollected();
     }
 }
