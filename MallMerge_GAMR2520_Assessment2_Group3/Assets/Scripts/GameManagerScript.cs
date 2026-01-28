de724d917dd999d09f25b8c326a7f036d7e0d26a
Iker Cubillo <i.cubillo@opendeusto.es>
2026-01-28 13:29:34 +0000

    Destoy ga,e manager destroy

--- MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/GameManagerScript.cs
+++ MallMerge_GAMR2520_Assessment2_Group3/Assets/Scripts/GameManagerScript.cs
@@ -20,7 +20,6 @@ public class GameManagerScript : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
